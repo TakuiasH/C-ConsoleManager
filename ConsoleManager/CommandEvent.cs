e9b52e9c349fd93f003774bfccc77232358affde
TakuiasH <arthsena.m@gmail.com>
2020-05-26 13:28:01 -0300

    My commit

---
+++ ConsoleManager/CommandEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandEvent {
+
+    public static void OnCommand(string[] args, string cmd)
+    {
+        if (EqualsIgnoreCase(cmd, "ping"))
+        {
+            Debug.Log("pong!");
+        }
+        else
+        {
+            Debug.LogError("[Error] Comando desconhecido.");
+        }
+    }
+
+
+    static bool EqualsIgnoreCase(string arg1, string arg2){return arg1.ToLower().Equals(arg2.ToLower());}
+}
